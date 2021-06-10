e48b9f1e1d3263a0bceb5323aa2627e6deb847e7
jxu <7989982+jxu@users.noreply.github.com>
2021-06-10 15:17:11 -0400

    Change word list to only lowercase words (avoid proper nouns #1)
    Former-commit-id: 94f49a1307f3b901468cf544ece97718476b1fd8

--- download_model.sh
+++ download_model.sh
@@ -3,7 +3,7 @@
 wget -c https://github.com/jxu/Word2VecDemo/releases/download/model/model.txt
 
 # take n many words (defined as only alpha characters, see issue #1)
-grep -P "[[:alpha:]]+ " model.txt | head -n10000 > wordvecs10k.txt
+grep -P "^[a-z]+ " model.txt | head -n10000 > wordvecs10k.txt
 
 # for analysis, get list of included words
 cut -d' ' -f1 wordvecs10k.txt > alphawords10k.txt
