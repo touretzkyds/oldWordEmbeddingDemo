23a39ee2b484012dde5cba4fd07117e3cc83487e
jxu <7989982+jxu@users.noreply.github.com>
2021-06-10 13:40:21 -0400

    Add included words list for analysis (#1)
    Former-commit-id: f7a6e966dd476f311c745887a3a94e5a451539b6

--- download_model.sh
+++ download_model.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # example of creating smaller model from NLPL model release
-
 wget -c https://github.com/jxu/Word2VecDemo/releases/download/model/model.txt
 
 # take n many words (defined as only alpha characters, see issue #1)
 grep -P "[[:alpha:]]+ " model.txt | head -n10000 > wordvecs10k.txt
 
+# for analysis, get list of included words
+cut -d' ' -f1 wordvecs10k.txt > alphawords10k.txt
