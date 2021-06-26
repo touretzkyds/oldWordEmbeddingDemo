51404b75ba0b1472380b07aa0ec3320a1fc1d144
jxu <7989982+jxu@users.noreply.github.com>
2021-06-25 23:54:19 -0400

    Cleanup nearest_words.py to be more flexible
    Former-commit-id: f25e37658f476b5f848b6045b5d38dbcca10aca8

--- nearest_words.py
+++ nearest_words.py
@@ -1,45 +1,53 @@
+# Compute top-k nearest neighbor words for each word in wordvecs model
+# Current algorithm is naive with O(n^2) comparisons,
+# O(log k) per comparison to maintain top-k list
+
 import numpy as np
 from heapq import *
+import sys
 
-DIM = 300  # vector dim
-N = 50000  # number of words
 K = 10  # top-k words
-words = [""] * N  # word index
-vecs = np.empty((N, DIM))  # words indexed by row
 
-# ensure model stays up to date with website!
-MODEL_PATH = "wordvecs50k.txt"
+def main():
+    MODEL_PATH = sys.argv[1]
 
-with open(MODEL_PATH) as f:
-    model_lines = f.readlines()
+    with open(MODEL_PATH) as f:
+        model_lines = f.readlines()
 
-assert len(model_lines) == N
+    N = len(model_lines)  # number of words
+    words = [""] * N  # word index
+    DIM = len(model_lines[0].strip().split()) - 1
+    assert DIM >= 50  # make sure we got rid of first line of fasttext format
+    vecs = np.empty((N, DIM))  # words indexed by row
 
-for i in range(N):
-    line = model_lines[i].strip().split()
-    words[i] = line[0]
-    assert len(line[1:]) == DIM
-    v = np.array(line[1:], dtype=np.float)
-    vecs[i,] = v / np.linalg.norm(v)  # normalize vecs
 
-# too much memory to store all vector distances, so compute on-the-fly
+    for i in range(N):
+        line = model_lines[i].strip().split()
+        words[i] = line[0]
+        v = np.array(line[1:], dtype=np.float)
+        vecs[i,] = v / np.linalg.norm(v)  # normalize vecs
 
-for i in range(N):
-    # compute all dot products (cos similarities) for word i vs all other words
-    sims = vecs @ vecs[i,]
 
-    # create (sim, index) key-value pairs, excluding word i itself
-    pairs = [(sims[j], j) for j in range(N) if j != i]
+    # too much memory to store all vector distances, so compute on-the-fly
 
-    # maintain top-k largest similarities using a *min* heap
-    # continuously remove min element, at the end we have all the max elements
-    top_pairs = []
-    for pair in pairs:
-        if len(top_pairs) < K:  # heap isn't full yet
-            heappush(top_pairs, pair)
-        elif pair > top_pairs[0]:
-            heapreplace(top_pairs, pair)
+    for i in range(N):
+        # compute all dot products (cos similarities) for word i vs all other words
+        sims = vecs @ vecs[i,]
 
-    nearest_words = [words[pair[1]] for pair in nlargest(K, top_pairs)]
-    print(words[i], " ".join(map(str, nearest_words)))
+        # create (sim, index) key-value pairs, excluding word i itself
+        pairs = [(sims[j], j) for j in range(N) if j != i]
 
+        # maintain top-k largest similarities using a *min* heap
+        # continuously remove min element, at the end we have all the max elements
+        top_pairs = []
+        for pair in pairs:
+            if len(top_pairs) < K:  # heap isn't full yet
+                heappush(top_pairs, pair)
+            elif pair > top_pairs[0]:
+                heapreplace(top_pairs, pair)
+
+        nearest_words = [words[pair[1]] for pair in nlargest(K, top_pairs)]
+        print(words[i], " ".join(map(str, nearest_words)))
+
+
+main()
