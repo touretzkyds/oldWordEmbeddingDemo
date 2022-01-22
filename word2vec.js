fac4e31588294dda95fd91a5f15cd348d878fc2f
Neel Pawar <81655816+neelpawarcmu@users.noreply.github.com>
2022-01-22 09:23:48 -0500

    improve hover output format of vector display (#41)

--- word2vec.js
+++ word2vec.js
@@ -386,6 +386,12 @@ class Demo {
         // heatmap plots matrix of values in z
         const z = this.vectorWords.map(word => this.vecs.get(word));
 
+        // improve hover output format of vector display (#41)
+        const hovertemplate = 'item: %{y}' +
+                '<br>dim: %{x}' +
+                '<br>value: %{z}' +
+                '<extra></extra>'; // removes trace name tag from second box of hovertemplate
+        
         const data = [
             {
                 // can't use y: this.vectorWords since the heatmap won't display duplicate words
@@ -393,7 +399,8 @@ class Demo {
                 zmin: this.HEATMAP_MIN,
                 zmax: this.HEATMAP_MAX,
                 type: "heatmap",
-                ygap: 5
+                ygap: 5,
+                hovertemplate: hovertemplate
             }
         ];
 
