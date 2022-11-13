d34e01e16baec416b9ac7bd609aa931111057d7d
Dave Touretzky <dst@cs.cmu.edu>
2022-11-13 05:31:16 -0500

    Added a figure

---
+++ figures/fig2a.m
@@ -0,0 +1,12 @@
+  fig1
+
+  dot2(w_adult)
+  dot2(w_child)
+  dot2(w_infant)
+  dot2(w_grandfather)
+  dot2(w_grandmother)
+  dot2(w_octogenarian)
+  dot2(w_grandparent)
+  dot2(w_teenager)
+
+  savefigure("fig2a.png")
--- figures/words2.m
+++ figures/words2.m
@@ -5,9 +5,13 @@ w_woman   =     {9, 7, 'woman'};
 w_boy     =     {1, 2, 'boy'};
 w_girl    =     {9, 2, 'girl'};
 w_adult =       {5, 7, 'adult'};
+w_teenager =    {5, 5, 'teenager'};
 w_child =       {5, 2, 'child'};
 w_infant =      {5, 1, 'infant'};
 w_grandfather = {1, 9, 'grandfather'};
+w_grandparent = {5, 9, 'grandparent'};
+w_grandmother = {9, 9, 'grandmother'};
+w_octogenarian = {5, 9.5, 'octogenarian'};
 
 words = [w_man; w_woman; w_boy; w_girl; ...
 	      w_adult; w_child; w_infant; w_grandfather];
